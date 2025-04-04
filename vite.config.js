7be97ee6024e422af07011c8e64ab524fdd21d16
Lee hwan seuk <sioo1014@naver.com>
2025-04-05 01:14:58 +0900

    chore: Prettier & EsLint 세팅(#1)

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import {fileURLToPath} from 'url';
+import { fileURLToPath } from 'url';
 import path from 'path';
 
 const __filename = fileURLToPath(import.meta.url);
@@ -11,16 +11,16 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@assets": path.resolve(__dirname, "./src/assets"),
-      "@components": path.resolve(__dirname, "./src/components"),
-      "@layout": path.resolve(__dirname, "./src/layout"),
-      "@pages": path.resolve(__dirname, "./src/pages"),
+      '@assets': path.resolve(__dirname, './src/assets'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@layout': path.resolve(__dirname, './src/layout'),
+      '@pages': path.resolve(__dirname, './src/pages'),
     },
     server: {
       port: 3000,
     },
     build: {
-      outDir: "build",
+      outDir: 'build',
     },
   },
-})
+});
