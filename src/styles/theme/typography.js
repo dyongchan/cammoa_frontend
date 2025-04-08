fac6e7b7763c626099ccd87317c4924ac612f7e1
Lee hwan seuk <sioo1014@naver.com>
2025-04-08 10:31:29 +0900

    chore(styled): typography 수정 (#5)

--- src/styles/theme/typography.js
+++ src/styles/theme/typography.js
@@ -1,8 +1,9 @@
 import { css } from 'styled-components';
 
+//타이포 정리
 const bodyBaseFontStyles = css`
-  font-weight: 700;
-  line-height: 100%;
+  font-family: 'Pretendard Variable';
+  line-height: 120%;
 `;
 
 const bodyStyles = {
@@ -12,29 +13,41 @@ const bodyStyles = {
   `,
   Body2: css`
     ${bodyBaseFontStyles}
-    font-size: 24px;
-    letter-spacing: 0.72px;
+    font-size: 28px;
+    font-weight: 700;
+    letter-spacing: -1px;
   `,
   Body3: css`
     ${bodyBaseFontStyles}
-    font-size: 21px;
+    font-size: 24px;
+    letter-spacing: 0.72px;
   `,
   Body4: css`
+    ${bodyBaseFontStyles}
+    font-size: 21px;
+  `,
+  Body5: css`
     ${bodyBaseFontStyles}
     font-size: 18px;
     letter-spacing: 0.36px;
   `,
-  Body5: css`
-    ${bodyBaseFontStyles}
-    font-size: 16px;
-  `,
   Body6: css`
     ${bodyBaseFontStyles}
-    font-size: 14px;
+    font-size: 16px;
+    font-weight: 700;
+    letter-space: -0.3px;
   `,
   Body7: css`
     ${bodyBaseFontStyles}
+    font-size: 14px;
+    font-weight: 400;
+    letter-spacing: -0.6px;
+  `,
+  Body8: css`
+    ${bodyBaseFontStyles}
     font-size: 12px;
+    font-weight: 700;
+    letter-space: -0.3px;
   `,
 };
 
