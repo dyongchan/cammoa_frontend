5d5c529812afcd188a4b3196e64c8803eb56752c
Lee hwan seuk <sioo1014@naver.com>
2025-04-28 20:55:43 +0900

    feat(home): 배너 구현(#10)

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,39 @@
-import theme from '@styles/theme/theme';
+import styled from 'styled-components';
+import BannerIcon from '@assets/icons/png/banner.png';
 
 const Home = () => {
-  console.log(theme.fontStyles.Body1);
-  return <h1></h1>;
+  return (
+    <HomeContainer>
+      <BannerContainer>
+        <BannerImage src={BannerIcon} alt="banner" />
+      </BannerContainer>
+    </HomeContainer>
+  );
 };
 
 export default Home;
+
+const HomeContainer = styled.div`
+  width: 100%;
+  padding: 155px 435px 0 435px;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 10px;
+`;
+
+const BannerContainer = styled.div`
+  width: 1100px;
+  height: fit-content;
+  padding-top: 10px;
+  margin-bottom: 10px;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+const BannerImage = styled.img`
+  width: fit-content;
+  height: fit-content;
+`;
